4615bf3941a05c985430a25f6a6c6617f7aa642d
cam <1743982333@qq.com>
2019-11-12 08:56:40 +0800

    add assets

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+/* <link rel="stylesheet" href="./lieliu_files/awesome.min.css">
+<link rel="stylesheet" href="./lieliu_files/bootstrap.css">
+<link rel="stylesheet" href="./lieliu_files/animate.min.css">
+<link rel="stylesheet" href="./lieliu_files/font_250416_xrr6wv8ab8.css">
+<link rel="stylesheet" href="./lieliu_files/app.css"></link> */
+import "./assests/lieliu_files/awesome.min.css";
+import "./assests/lieliu_files/bootstrap.css";
+import "./assests/lieliu_files/animate.min.css";
+import "./assests/lieliu_files/font_250416_xrr6wv8ab8.css";
+import "./assests/lieliu_files/app.css";
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    el: "#app",
+    router,
+    render: h => h(App)
+});
