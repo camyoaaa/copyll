840dbe58ea90e873a6e155036a4cb695b7b2fbc7
cam <1743982333@qq.com>
2019-11-22 18:02:43 +0800

    xxx

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 // import ElementUI from 'element-ui';
 import './elementImport.js';
 import './antvueImport.js';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import Velocity from 'velocity-animate';
 import App from "./App.vue";
 import router from "./router";
 import "./assests/lieliu_files/animate.min.css";
@@ -10,12 +12,16 @@ import "./plugins/element.js";
 import "./plugins/ant-design-vue.js";
 import moment from "moment";
 import VueJsonp from "vue-jsonp";
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper);
 Vue.use(VueJsonp);
 window.Jsonp = Vue.jsonp;
 // Vue.use(ElementUI);
 Vue.config.productionTip = false;
 Vue.prototype.$log = window.console.log;
 Vue.prototype.$moment = moment;
+Vue.prototype.$velocity = Velocity;
 new Vue({
   el: "#app",
   router,
